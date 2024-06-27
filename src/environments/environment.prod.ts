f20558b3f3294f78bd2c94c2125a8022c1abb8a9
Somadina Eze <140349370+somadina6@users.noreply.github.com>
2024-06-27 12:28:47 +0000

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    API_URL: 'https://transaction-app-server.vercel.app/'
+    API_URL: 'https://transaction-app-server.vercel.app/transactions'
   };
   
