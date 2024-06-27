5fad7a515296e02b2ff0f35b33292b665cf5a331
Somadina Eze <140349370+somadina6@users.noreply.github.com>
2024-06-27 12:25:46 +0000

    update url

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { TransactionDataType } from '../types/transactions.type';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
